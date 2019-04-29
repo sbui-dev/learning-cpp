f76e15b5a8c09655806a66b7950a9bcdaf3e16f0
Steven Bui <sbui.513@gmail.com>
2019-04-29 16:46:34 -0700

    cpp11 constructor, rvalue, and lvalue practice

---
+++ CoP/cpp11-constructors/cpp11-constructors.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Parent {
+    int dogs{ 5 };
+    string text;
+public:
+    
+    // delegating constructor
+    // c++11 will allow a constructor to call another constructor
+    // c++98 used a "init()" to get by this
+    Parent() : Parent("hello") {
+        cout << "no parameter parent constructor" << endl;
+    }
+
+    // c++98: creating a constructor will lose the default constructor
+    Parent(string text) {
+        this->text = text;
+        cout << "string parent constructor" << endl;
+    }
+};
+
+class Child : public Parent {
+public:
+    // c++98: can select which constructor to use
+    //Child(): Parent("hello") {
+    //}
+
+    // c++11: use default constructor
+    Child() = default;
+};
+
+class Test {
+private:
+    static const int SIZE = 100;
+    int* pBuffer;
+public:
+    Test() {
+        cout << "constructor" << endl;
+        pBuffer = new int[SIZE] {};
+    }
+
+    Test(int i) {
+        cout << "parameterized constructor" << endl;
+        pBuffer = new int[SIZE] {};
+
+        for (int i = 0; i < SIZE; i++) {
+            pBuffer[i] = 7 * 1;
+        }
+    }
+
+    Test(const Test& other) {
+        cout << "copy constructor" << endl;
+        pBuffer = new int[SIZE] {};
+
+        memcpy(pBuffer, other.pBuffer, SIZE * sizeof(int));
+    }
+
+    ~Test() {
+        cout << "destructor" << endl;
+        delete[] pBuffer;
+    }
+
+    Test& operator=(const Test& other) {
+        cout << "assignment" << endl;
+        pBuffer = new int[SIZE] {};
+
+        memcpy(pBuffer, other.pBuffer, SIZE * sizeof(int));
+        return *this;
+    }
+};
+
+ostream& operator<<(ostream& out, const Test& test) {
+    out << "Hello from test";
+    return out;
+}
+
+Test getTest() {
+    return Test();
+}
+
+void delegatingContructor() {
+    // see notes in classes
+    Parent parent("Hello");
+    Child child;
+}
+
+void constructorMemory() {
+
+    // return value of function getTest() is a rvalue
+    Test test1 = getTest();
+
+    cout << test1 << endl;
+
+    vector<Test> vec;
+    vec.push_back(Test());
+
+    // rvalue example
+    int value1 = 7;
+
+    // lvalue example
+    int* pValue1 = &value1;
+
+    // lvalue example
+    // this will work
+    int* pValue3 = &++value1;
+    cout << *pValue3 << endl;
+
+    // rvalue example
+    // this will not work; post fix is operating on a temporary value
+    //int* pValue4 = &value1++;
+}
+
+void lValueRef() {
+
+    Test test1 = getTest();
+    
+    Test &lTest1 = test1;
+
+    // can't bind lValue to temporary rvalues
+
+    // can't work return value is a temporary value
+    //Test& rTest2 = getTest();
+
+    // const will make lvalue bind to rvalues
+    const Test& rTest3 = getTest();
+
+    // Test() is an rvalue but it gets copied
+    Test test2(Test(1));
+}
+
+void check(const Test& value) {
+    cout << "lValue function" << endl;
+}
+
+void check(Test&& value) {
+    cout << "rValue function" << endl;
+}
+
+void rValueRef() {
+    Test test1 = getTest();
+
+    cout << test1 << endl;
+
+    Test& lTest1 = test1;
+
+    // can bind rvalue to rvalue
+    Test&& rtest1 = Test();
+
+    Test&& rtest2 = getTest();
+
+    check(test1); //lvalue
+    check(getTest()); // rvalue
+    check(Test()); // rvalue
+}
+
+int main() {
+    //delegatingContructor();
+    //constructorMemory();
+    //lValueRef();
+    rValueRef();
+    return 0;
+}
