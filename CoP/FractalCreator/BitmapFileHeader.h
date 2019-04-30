111beb7548c6a6ff42742f3cb4ddbcbd7f6aaf46
Steven Bui <sbui.513@gmail.com>
2019-04-29 22:08:33 -0700

    Initial Fractal Creator project with some bitmap header defined

---
+++ CoP/FractalCreator/BitmapFileHeader.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+
+using namespace std;
+
+#pragma pack(2)
+struct BitmapFileHeader {
+    char header[2]{ 'B', 'M' };
+    int32_t fileSize;
+    int32_t reserved{ 0 };
+    int32_t dataOffset;
+};
