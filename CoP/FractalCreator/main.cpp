edc1e360794244fed6d015902b9ae5477b880754
Steven Bui <sbui.513@gmail.com>
2019-04-30 01:34:27 -0700

    build a color histogram

--- CoP/FractalCreator/main.cpp
+++ CoP/FractalCreator/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <memory>
 #include "Bitmap.h"
 #include "Mandelbrot.h"
 
@@ -16,18 +17,27 @@ int main()
 
     double min = 9999999;
     double max = -9999999;
+
+    unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS]{});
+
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            double xFractal = (x - WIDTH / 2) * 2.0 / WIDTH;
+            double xFractal = (x - WIDTH / 2 - 200) * 2.0 / HEIGHT;
             double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
 
             int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-            uint8_t red = static_cast<uint8_t>(256 * static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS);
+            if (iterations != Mandelbrot::MAX_ITERATIONS) {
+                histogram[iterations]++;
+            }
 
-            bitmap.setPixel(x, y, red, red, red);
-            if (red < min) min = red;
-            if (red > max) max = red;
+            uint8_t color = static_cast<uint8_t>(256 * static_cast<double>(iterations) / Mandelbrot::MAX_ITERATIONS);
+
+            color = color * color * color;
+
+            bitmap.setPixel(x, y, 0, color, 0);
+            if (color < min) min = color;
+            if (color > max) max = color;
         }
     }
 
